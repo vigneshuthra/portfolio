c5adad8fa0b0a389915c2e461541c65947729589
Vignesh Uthrapathy <52934902+vigneshuthra@users.noreply.github.com>
2024-11-07 16:15:57 +0000

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from "../assets/Uv-logo.png";
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <img src={logo} alt='logo' className='mx-2 w-10'></img>
+
+      </div>
+      <div className='flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin/>
+        <FaGithub/>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
