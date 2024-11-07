306ddcae50cc581772c5dbe2876c666dae09b58e
Vignesh Uthrapathy <52934902+vigneshuthra@users.noreply.github.com>
2024-11-07 22:58:55 +0000

    added Framer motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Get in Touch</h2>
-      <div className="text-center tracking-tighter">
+      <motion.h2 
+      whileInView={{opacity:1, y:0}}
+      initial={{opacity:0, y:-100}}
+      transition={{duration:1.5}}
+      className="my-20 text-center text-4xl">Get in Touch</motion.h2>
+      <motion.div
+      whileInView={{opacity:1, x:0}}
+      initial={{opacity:0, x:-100}}
+      transition={{duration:1.5}}
+       className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.phoneNo}</p>
         <a href="#" className="border-b">
           {CONTACT.email}
         </a>
         <p></p>
-      </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,35 +1,85 @@
 import React from "react";
 import { FaHtml5, FaNodeJs } from "react-icons/fa";
-import { RiNextjsLine, RiReactjsLine } from "react-icons/ri";
+import { RiReactjsLine } from "react-icons/ri";
 import { SiAngular, SiMongodb } from "react-icons/si";
-import { TbBrandAngular, TbBrandNextjs } from "react-icons/tb";
+import { TbBrandNextjs } from "react-icons/tb";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  intial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <RiReactjsLine className="text-7xl text-cyan-400"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <TbBrandNextjs className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiAngular className="text-7xl text-red-700"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <FaNodeJs className="text-7xl text-green-500"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <FaHtml5 className="text-7xl text-orange-600"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiMongodb className="text-7xl text-green-400"/>
-        </div>
-       
-       
-      </div>
+      <motion.h2 
+       whileInView={{opacity:1, y:0}}
+       initial={{opacity:0, y:-100}}
+       transition={{duration:1.5}}
+       className="my-20 text-center text-4xl">Technologies</motion.h2>
+      <motion.div
+      whileInView={{opacity:1, x:0}}
+      initial={{opacity:0, x:-100}}
+      transition={{duration:1.5}}
+      className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div
+          variants={iconVariants(2.5)}
+          intial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          intial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandNextjs className="text-7xl" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          intial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiAngular className="text-7xl text-red-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          intial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          intial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaHtml5 className="text-7xl text-orange-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.5)}
+          intial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-400" />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
