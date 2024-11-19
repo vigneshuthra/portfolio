caf0b7a9969fff29e9e1888a6b36df8368160641
Vignesh Uthrapathy <52934902+vigneshuthra@users.noreply.github.com>
2024-11-19 18:24:24 +0000

    minor fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
+    <div className="border-b border-neutral-900 mt-10 lg:mt-0 pb-4">
       <h1 className="my-20 text-center text-4xl">
         About
         <span className="text-neutral-500"> Me</span>
